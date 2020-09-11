e67d8d2eeb89d7d610df4f8861eb8825eef4881c
huynhhocgioi <71130148+huynhhocgioi@users.noreply.github.com>
2020-09-11 23:12:45 +0700

    lad1

---
+++ ss2/vd1.c
@@ -0,0 +1,17 @@
+#include  <stdio.h>
+int main( void ){
+	int a;
+	int b;
+	int c;
+	
+	printf("Enter a\n");
+	scanf("%d" , &a );
+	
+	printf("Enter b\n");
+	scanf("%d" , &b );
+	
+	c = a + b ;
+	printf(" c = %d " , c );
+	
+	return 0;
+}
---
+++ ss3/vd3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	float x , y;
+	
+	printf("Nhap vao mot so bat ki :\n");
+	scanf("%f", &x);
+	
+	y = x * x ;
+	printf("Y = %f\n", y);
+	return 0;
+}
